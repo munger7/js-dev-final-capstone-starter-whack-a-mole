f0070d663900917362c86e37fe8e0934466b0c54
Mattison Unger <mattisonunger123@gmail.com>
2024-02-12 14:06:28 -0800

    tests pass US-01 - US-03

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const holes = document.querySelectorAll('.hole');
 const moles = document.querySelectorAll('.mole');
 const startButton = document.querySelector('#start');
 // TODO: Add the missing query selectors:
-const score; // Use querySelector() to get the score element
-const timerDisplay; // use querySelector() to get the timer element.
+const score = document.querySelector("#score"); // Use querySelector() to get the score element
+const timerDisplay = document.querySelector("#timer"); // use querySelector() to get the timer element.
 
 let time = 0;
 let timer;
@@ -21,7 +21,7 @@ let difficulty = "hard";
  *
  */
 function randomInteger(min, max) {
-  // return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 /**
@@ -41,7 +41,9 @@ function randomInteger(min, max) {
  */
 function setDelay(difficulty) {
   // TODO: Write your code here.
-  
+  if(difficulty === "easy") return 1500;
+  else if (difficulty === "normal") return 1000;
+  else return randomInteger(600, 1200);  
 }
 
 /**
@@ -60,7 +62,10 @@ function setDelay(difficulty) {
  */
 function chooseHole(holes) {
   // TODO: Write your code here.
-
+  // 1. Generate a random integer from 0 to 8 and assign it to an index variable.
+   // 2. Get a random hole with the random index (e.g., const hole = holes[index]).
+   // 3. if hole === lastHole, then call chooseHole(holes) again because you don't want to return the same hole.
+   // 4. if hole is not the same as the lastHole, then keep track of it (lastHole = hole) and return the hole.
 }
 
 /**
